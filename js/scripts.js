c12257b0e3ba52e9ce1baefde4944665e2613ec8
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-08-03 14:54:50 +0300

    add computation

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,50 @@
   //Business Logic
-  document.getElementById("submit").addEventListener("click", PizzaOrder.pizzaOrderCost);
-  function PizzaOrder(size, crust, toppings, quantity, delivery){
-    // this.pizzaSize=size;
-    // this.pizzaCrust=crust;
-    // this.pizzaToppings=toppings;
-    // this.pizzaQuantity=quantity
-    // this.pizzaDelivery=delivery
+  // function PizzaOrder(size){
+  //   this.inputSize=size
+  // };
+var order=[];
+  const myFunction = (ev)=>{
+    ev.preventDefault();
+    //var order=[];
+    var size=document.getElementById('pizzaSize').value;
+    var crust=document.getElementById("crust").value;
+    var toppings=document.getElementById("toppings").value;
+    var quantity=document.getElementById("quantity").value;
+    var delivery=document.getElementById("delivery").value;
 
-  this.document.querySelector("#pizzaSize").value=size;
-  this.document.querySelector("#crust").value()=crust;
-  this.document.querySelector("#toppings").value()=toppings;
-  this.document.querySelector("#quantity").value()=quantity ;
-  this.document.querySelector("#delivery").value()=delivery ;
+    order.push(size, crust, toppings, quantity, delivery);
+    alert("This is your order" + " "+ order);
   }
+
+// ********************************************************************
+//Failed to invoke the prototype function. No time to research further.
+//***********************************************************************
+
+var listPrice=[1000, 1500, 2000, 100, 200, 400, 200,400, 600]
+function myFunctiontwo(){
+  if (order[0]==='Small' && order[1]==='Crispy Crusty' && order[2]==='Veggy' && order[4]==='Yes' 
+  && order[3]==='1'){
+    // alert('Your Bill is :\n' + 'Size' + (order[0] + '\n' + (parseInt(listPrice[0])+ parseInt(listPrice[3])))
+    alert('Your bill is :\n '+ 'Size' + ' '+ order[0] + ' '+ listPrice[0])
+  } 
+}
+
+
+  document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById("delivery").addEventListener("input", myFunction);
+
+    document.getElementById("submit").addEventListener("click", myFunctiontwo);
+  });
   
-  PizzaOrder.prototype.pizzaOrderCost = function(){
-   alert('testing');
-  }
-
-
 
 
 
 //  ****************** 
-var size = document.getElementById("pizzaSize").value;
-//document.getElementById("submit").addEventListener("click", myFunction);
-function myFunction() {
-  alert("Your Pizza size is" + " " + size)
-}
+// var size = document.getElementById("pizzaSize").value;
+// //document.getElementById("submit").addEventListener("click", myFunction);
+// function myFunction() {
+//   alert("Your Pizza size is" + " " + size)
+// }
 // ******************
   //User Logic
 
